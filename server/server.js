efa6b42f75aea92d10b0982d22b06b1e28ff175b
SandScanner <144828787+SandScanner@users.noreply.github.com>
2023-10-23 16:11:54 +0530

    Update server.js
    connection timeout

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ let db = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
+    connectTimeout: 30000
 })
 
 app.use(cors());
