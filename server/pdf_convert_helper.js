6725d3941c2171eac239e1b64de380a116b14a39
Parameshwar <spaparameshwar003@gmail.com>
2024-01-27 16:17:04 +0530

    font paths updated

--- server/pdf_convert_helper.js
+++ server/pdf_convert_helper.js
@@ -7,7 +7,20 @@ async function generatePdf(html) {
     await page.setContent(html);
   
     // Generate PDF buffer
-    const pdfBuffer = await page.pdf({ width: '207px', height: '740px'});
+    const pdfBuffer = await page.pdf({ width: '207px', height: '740px'
+    , fontMetrics: [
+      {
+        family: 'Nirmala UI',
+        src: '/opt/render/.fonts/Nirmala.ttf', // Replace with the path to your font file
+      }, {
+        family: 'Nirmala UI Semilight',
+        src: '/opt/render/.fonts/NirmalaS.ttf', // Replace with the path to your font file
+      },{
+        family: 'Nirmala UI Bold',
+        src: '/opt/render/.fonts/NirmalaB.ttf', // Replace with the path to your font file
+      }
+    ],
+   });
   
     await browser.close();
   
